f82a2c8eb562af787b5df8c42db581dd1458a347
CiarenceW <112902813+CiarenceW@users.noreply.github.com>
2025-11-04 03:51:36 +0100

    Remove unused build configs, move more stuff to Directory.Build.props, explicitly add Mono.Cecil 0.11.6 as package reference

--- build.cmd
+++ build.cmd
@@ -2,8 +2,4 @@ set "CONFIG=-c Debug"
 
 IF "%~1" NEQ "" set "CONFIG=-c %1"
 
-dotnet build "./BepInSbox.Core/BepInSbox.Core.csproj" %CONFIG%
-dotnet build "./BepInSbox.Preloader.Core/BepInSbox.Preloader.Core.csproj" %CONFIG%
-dotnet build "./Runtimes/NET/BepInSbox.NET.Common/BepInSbox.NET.Common.csproj" %CONFIG%
-dotnet build "./Runtimes/NET/BepInSbox.NET.CoreCLR/BepInSbox.NET.CoreCLR.csproj" %CONFIG%
-
+dotnet build "./BepInSbox.sln" %CONFIG%
